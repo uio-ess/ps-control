a91285e8a94f725911d89aa9a0a596f9ccdbacd9
Grey Christoforo <grey@christoforo.net>
2018-12-12 13:28:02 +0000

    default voltage range up to +/- 2V

--- ps4262.py
+++ ps4262.py
@@ -20,7 +20,7 @@ class ps4262:
     # for AWG
     nWaveformSamples = 2 ** 12
     waveform = np.zeros(nWaveformSamples)
-    def __init__(self, VRange = 1, requestedSamplingInterval = 1e-6, tCapture = 0.3, triggersPerMinute = 30):
+    def __init__(self, VRange = 2, requestedSamplingInterval = 1e-6, tCapture = 0.3, triggersPerMinute = 30):
         """
         picotech PS4262 library constructor
         """
@@ -162,7 +162,7 @@ class ps4262:
                 self.singleShotMode = True
             future = self.executor.submit(self.ps._lowLevelSigGenSoftwareControl,0)
 
-    def _setChannel(self, VRange = 1):
+    def _setChannel(self, VRange = 2):
         self.VRange = VRange
         future = self.executor.submit(self.ps.setChannel, channel='A', coupling='DC', VRange=VRange, VOffset=0.0, enabled=True, BWLimited=0, probeAttenuation=1.0)
 
